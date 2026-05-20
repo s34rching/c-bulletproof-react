9d97ea585993995275c0b1316312243ec826a515
Ivan Katsenia <ivan.katsenya@gmail.com>
2026-05-20 20:35:03 +0200

    Extend 'Hero' test suite

--- apps/nextjs-app/e2e/pages/hero.page.ts
+++ apps/nextjs-app/e2e/pages/hero.page.ts
@@ -5,11 +5,13 @@ import { type Locator, type Page } from '@playwright/test';
 export class HeroPage extends BasePage {
   readonly page: Page;
   readonly getStartedButton: Locator;
+  readonly alanRepoButton: Locator;
 
   constructor(page: Page) {
     super(page);
 
     this.page = page;
     this.getStartedButton = page.getByRole('button', { name: 'Get Started' });
+    this.alanRepoButton = page.getByRole('button', { name: 'Github Repo' });
   }
 }
--- apps/nextjs-app/e2e/tests/hero.spec.ts
+++ apps/nextjs-app/e2e/tests/hero.spec.ts
@@ -2,10 +2,17 @@ import { expect, test } from '../fixtures/pages';
 import { createUser } from '@/testing/data-generators';
 
 test.describe('"Hero" page', () => {
-  test('user should be able to open auth page', async ({ heroPage }) => {
+  test.beforeEach(async ({ heroPage }) => {
     await heroPage.open('/');
+  })
+
+  test('user should be able to open auth page', async ({ heroPage }) => {
     await heroPage.getStartedButton.click();
     await heroPage.waitUrlContains(/\/auth\/login$/);
     await expect(heroPage.getStartedButton).toBeHidden();
-  })
+  });
+
+  test('user should be able to see author\'s "Github" profile', async ({ heroPage }) => {
+    await expect(heroPage.alanRepoButton).toBeVisible();
+  });
 })
