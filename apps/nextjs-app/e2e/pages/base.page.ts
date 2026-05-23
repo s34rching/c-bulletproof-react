f893bd12d59771a89210f34446c7fbae1f9f4d27
Ivan Katsenia <ivan.katsenya@gmail.com>
2026-05-23 17:47:39 +0200

    CBR-1: Register user via API

--- apps/nextjs-app/e2e/pages/base.page.ts
+++ apps/nextjs-app/e2e/pages/base.page.ts
@@ -1,4 +1,7 @@
-import { type Locator, type Page } from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
+import { generateUser } from '@/testing/data-generators';
+
+const USER_SETTLE_TIMEOUT = 2000;
 
 export class BasePage {
   readonly page: Page;
@@ -15,5 +18,37 @@ export class BasePage {
 
   async waitUrlContains(path: string | RegExp) {
     await this.page.waitForURL(path);
+  }
+
+  async waitForComplete(operation: string, timeout: number): Promise<void> {
+    setTimeout(async () => {
+      return new Promise((resolve) => {
+        console.log(`Waiting ${timeout}ms for "${operation}" to complete`);
+
+        resolve();
+      });
+    }, USER_SETTLE_TIMEOUT);
+  }
+
+  async createRegisteredUser(userData: Partial<ReturnType<typeof generateUser>>): Promise<void> {
+    const { firstName, lastName, email, password, teamName } = userData;
+
+    const response = await this.page.request.post(`http://localhost:8080/api/auth/register`, {
+      data: {
+        firstName,
+        lastName,
+        email,
+        password,
+        teamName,
+        teamId: null,
+      },
+    });
+    expect(response.status()).toBe(200)
+
+    await this.waitForComplete('User is being set in system', USER_SETTLE_TIMEOUT);
+  }
+
+  async resetPageState(): Promise<void> {
+    await this.page.context().clearCookies();
   }
 }
--- apps/nextjs-app/e2e/tests/login.spec.ts
+++ apps/nextjs-app/e2e/tests/login.spec.ts
@@ -1,82 +1,112 @@
 import { expect, test } from '../fixtures/pages';
-import { createUser } from '@/testing/data-generators';
+import { createUser, generateUser } from '@/testing/data-generators';
 
 test.describe('"Login" page', () => {
-  const credentials = createUser();
+  test.describe('Registered user', () => {
+    let userData: ReturnType<typeof generateUser>;
 
-  test('TC-001: User should be able to login with valid credentials', async ({ loginPage }) => {
-    await loginPage.open('/auth/login');
-    await loginPage.emailInput.fill(credentials.email);
-    await loginPage.passwordInput.fill(credentials.password);
-    await loginPage.loginButton.click();
+    test.beforeEach(async ({ loginPage }) => {
+      userData = createUser();
 
-    await loginPage.waitUrlContains(/\/app/);
-    await expect(loginPage.emailInput).toBeHidden();
-  });
+      await loginPage.createRegisteredUser(userData);
+      await loginPage.resetPageState();
+      await loginPage.open('/auth/login');
+    });
 
-  test('TC-005: User should see validation error while submitting empty email', async ({ loginPage }) => {
-    await loginPage.open('/auth/login');
-    await loginPage.passwordInput.fill(credentials.password);
-    await loginPage.loginButton.click();
-    await expect(loginPage.emailInputAlert).toBeVisible();
-    await expect(loginPage.emailInputAlert).toContainText('Required');
-  });
+    test('TC-001: User should be able to login with valid credentials', async ({
+      loginPage,
+    }) => {
+      await loginPage.emailInput.fill(userData.email);
+      await loginPage.passwordInput.fill(userData.password);
+      await loginPage.loginButton.click();
 
-  test('TC-006: User should see validation error while submitting email in wrong format', async ({
-    loginPage
-  }) => {
-    const withoutAtEmail = 'some.address'
+      await loginPage.waitUrlContains(/\/app/);
+      await expect(loginPage.emailInput).toBeHidden();
+    });
 
-    await loginPage.open('/auth/login');
-    await loginPage.emailInput.fill(withoutAtEmail);
-    await loginPage.passwordInput.fill(credentials.password);
-    await loginPage.loginButton.click();
+    test('TC-018: User should be able to submit login with "Enter"', async ({
+      loginPage,
+    }) => {
+      await loginPage.emailInput.fill(userData.email);
+      await loginPage.passwordInput.fill(userData.password);
+      await loginPage.passwordInput.press('Enter');
 
-    const message = await loginPage.emailInput
-      .evaluate((el: HTMLInputElement) => el.validationMessage);
+      await loginPage.waitUrlContains(/\/app/);
+      await expect(loginPage.emailInput).toBeHidden();
+    });
 
-    expect(message).toContain(withoutAtEmail);
-  });
+    test('TC-009: User should NOT be able to login with wrong password', async ({
+      loginPage,
+    }) => {
+      const { password: randomPassword } = createUser();
 
+      await loginPage.emailInput.fill(userData.email);
+      await loginPage.passwordInput.fill(randomPassword);
+      await loginPage.loginButton.click();
+      await expect(loginPage.eventAlert).toBeVisible();
+      await expect(loginPage.eventAlert).toContainText(
+        'Invalid username or password',
+      );
+    });
+  })
 
-  test('TC-007: User should see validation error while submitting email in wrong format', async ({
-    loginPage,
-  }) => {
-    await loginPage.open('/auth/login');
-    await loginPage.emailInput.fill(credentials.email);
-    await loginPage.loginButton.click();
-    await expect(loginPage.passwordInputAlert).toBeVisible();
-    await expect(loginPage.passwordInputAlert).toContainText('Required');
-  });
+  test.describe('Validations', () => {
+    test('TC-005: User should see validation error while submitting empty email', async ({
+      loginPage,
+    }) => {
+      const { password } = createUser();
 
-  test('TC-009: User should NOT be able to login with wrong password', async ({
-    loginPage,
-  }) => {
-    const { password: randomPassword } = createUser();
+      await loginPage.open('/auth/login');
+      await loginPage.passwordInput.fill(password);
+      await loginPage.loginButton.click();
+      await expect(loginPage.emailInputAlert).toBeVisible();
+      await expect(loginPage.emailInputAlert).toContainText('Required');
+    });
 
-    await loginPage.open('/auth/login');
-    await loginPage.emailInput.fill(credentials.email);
-    await loginPage.passwordInput.fill(randomPassword);
-    await loginPage.loginButton.click();
-    await expect(loginPage.eventAlert).toBeVisible();
-    await expect(loginPage.eventAlert).toContainText(
-      'Invalid username or password',
-    );
-  });
+    test('TC-006: User should see validation error while submitting email in wrong format', async ({
+      loginPage,
+    }) => {
+      const { password } = createUser();
+      const withoutAtEmail = 'some.address';
 
-  test('TC-010: User should NOT be able to login with non-existing email', async ({
-    loginPage,
-  }) => {
-    const { email: nonExistingEmail, password } = createUser();
+      await loginPage.open('/auth/login');
+      await loginPage.emailInput.fill(withoutAtEmail);
+      await loginPage.passwordInput.fill(password);
+      await loginPage.loginButton.click();
 
-    await loginPage.open('/auth/login');
-    await loginPage.emailInput.fill(nonExistingEmail);
-    await loginPage.passwordInput.fill(password);
-    await loginPage.loginButton.click();
-    await expect(loginPage.eventAlert).toBeVisible();
-    await expect(loginPage.eventAlert).toContainText(
-      'Invalid username or password',
-    );
+      const message = await loginPage.emailInput.evaluate(
+        (el: HTMLInputElement) => el.validationMessage,
+      );
+
+      expect(message).toContain(withoutAtEmail);
+    });
+
+    test('TC-007: User should see validation error while submitting empty password', async ({
+      loginPage,
+    }) => {
+      const { email, password } = createUser();
+
+      await loginPage.open('/auth/login');
+      await loginPage.emailInput.fill(email);
+      await loginPage.loginButton.click();
+      await expect(loginPage.passwordInputAlert).toBeVisible();
+      await expect(loginPage.passwordInputAlert).toContainText('Required');
+    });
+
+    test('TC-010: User should NOT be able to login with non-existing email', async ({
+      loginPage,
+    }) => {
+      const { email: nonExistingEmail, password } = createUser();
+
+      await loginPage.open('/auth/login');
+      await loginPage.emailInput.fill(nonExistingEmail);
+      await loginPage.passwordInput.fill(password);
+      await loginPage.loginButton.click();
+      await expect(loginPage.eventAlert).toBeVisible();
+      await expect(loginPage.eventAlert).toContainText(
+        'Invalid username or password',
+      );
+    });
   });
 
   test('TC-013: User should be able to see "Register" link', async ({ loginPage }) => {
@@ -84,15 +114,5 @@ test.describe('"Login" page', () => {
 
     await expect(loginPage.registerLink).toBeVisible();
     await expect(loginPage.registerLink).toHaveAttribute('href', '/auth/register');
-  });
-
-  test('TC-018: User should be able to submit login with "Enter"', async ({ loginPage }) => {
-    await loginPage.open('/auth/login');
-    await loginPage.emailInput.fill(credentials.email);
-    await loginPage.passwordInput.fill(credentials.password);
-    await loginPage.passwordInput.press('Enter');
-
-    await loginPage.waitUrlContains(/\/app/);
-    await expect(loginPage.emailInput).toBeHidden();
   });
 });
