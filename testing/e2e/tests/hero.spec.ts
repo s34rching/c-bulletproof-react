f809c9967a7b2a1e5912d81ace7de528aea2e08e
Ivan Katsenia <ivan.katsenya@gmail.com>
2026-06-02 22:01:18 +0200

    CBR-018: Test

--- testing/e2e/tests/hero.spec.ts
+++ testing/e2e/tests/hero.spec.ts
@@ -3,15 +3,15 @@ import { expect, test } from '../fixtures/pages';
 test.describe('"Hero" page', () => {
   test.beforeEach(async ({ heroPage }) => {
     await heroPage.open('/');
-  })
+  });
 
   test('user should be able to open auth page', async ({ heroPage }) => {
     await heroPage.getStartedButton.click();
     await heroPage.waitUrlContains(/\/auth\/login$/);
     await expect(heroPage.getStartedButton).toBeHidden();
-  })
+  });
 
   test('user should be able to see author\'s "Github" profile', async ({ heroPage }) => {
     await expect(heroPage.alanRepoButton).toBeVisible();
-  })
-})
+  });
+});
